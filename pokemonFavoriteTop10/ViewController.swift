a88e42f6b198290dc320686729754fe9097be4aa
user 24 <user_24@mac-24deimac.local>
2016-10-31 15:24:27 +0800

    Initial Commit

---
+++ pokemonFavoriteTop10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokemonFavoriteTop10
+//
+//  Created by user_24 on 2016/10/31.
+//  Copyright © 2016年 user_24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
